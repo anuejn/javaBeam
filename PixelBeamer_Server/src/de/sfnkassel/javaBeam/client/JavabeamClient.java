c811532854bbea6b51a778cd1fa9bc31e88e1ff7
Jaro <jarohabiger@googlemail.com>
2015-06-26 19:32:53 +0200

    bevore merge

--- PixelBeamer_Server/src/de/sfnkassel/javaBeam/client/JavabeamClient.java
+++ PixelBeamer_Server/src/de/sfnkassel/javaBeam/client/JavabeamClient.java
@@ -1,7 +1,39 @@
 package de.sfnkassel.javaBeam.client;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class JavabeamClient {
-	public static void main(String[] args) {
-		new Integer(10).byteValue();
+	private URL ip;
+	
+	public JavabeamClient(URL ip) {
+		this.ip = ip;
 	}
+	
+	public JavabeamClient(String ip) throws MalformedURLException {
+		this.ip = new URL(ip);
+	}
+	
+	public void drawText(String text) {
+		
+	}
+	
+	private void sendToServer(byte[] bytes) throws IOException {
+		URLConnection connection = ip.openConnection();
+		connection.getOutputStream().write(bytes);
+		if(connection.getInputStream().read() == 0xAA) {
+			
+		} else {
+			throw new IOException();
+		}
+	}
+	
+	
+	public static final byte CMD_DRAW_PIXEL = 0x01;
+	public static final byte CMD_DRAW_RECTANGLE = 0x02;
+	public static final byte CMD_DRAW_LINE = 0x03;
+	public static final byte CMD_DRAW_CIRCLE = 0x04;
+	public static final byte CMD_DRAW_TEXT = 0x05;
 }
--- PixelBeamer_Server/src/de/sfnkassel/javaBeam/server/draw/Drawer.java
+++ PixelBeamer_Server/src/de/sfnkassel/javaBeam/server/draw/Drawer.java
@@ -40,24 +40,4 @@ public class Drawer {
 	public static final byte CMD_DRAW_TEXT = 0x05;
 	
 	public static final byte INVALID = 0x00;
-	
-	/*
-	 * Byte 1: Cmd -> 0x01 = Draw a Pixel
-	 * 				  0x02 = Draw a Rectangle
-	 * 				  0x03 = Draw a Line
-	 * 				  0x04 = Draw a Circle
-	 * 				  0x05 = Draw a Text
-	 * 
-	 * Byte 2 - 4: Color -> r, g, b
-	 * 
-	 * Byte 5 - 8: X-Pos. 1 | Int
-	 * 
-	 * Byte 9 - 12: Y-Pos. 1 | Int
-	 * 
-	 * Byte 13 - 20 (bei Rec, Line): X 2, Y 2 | Int
-	 * 
-	 * Byte 13 - 16 (bei Circle): Radius | Int
-	 * 
-	 * Byte 13f (je 2, bei Text): Chars | Char
-	 */
 }
