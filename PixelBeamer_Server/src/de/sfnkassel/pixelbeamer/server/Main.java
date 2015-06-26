0aa99fac9ef95177d507c9459dd097af42bc2714
Birk Magnussen <birk.magnussen@online.de>
2015-06-26 18:09:56 +0200

    Inital Commit - Project showing a Window with JavaFX

---
+++ PixelBeamer_Server/src/de/sfnkassel/pixelbeamer/server/Main.java
@@ -0,0 +1,29 @@
+package de.sfnkassel.pixelbeamer.server;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	private StackPane mainPane = new StackPane();
+	private Canvas drawCanvas;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("PixelBeamer");
+		drawCanvas = new Canvas(primaryStage.getWidth(), primaryStage.getHeight());
+		drawCanvas.widthProperty().bind(mainPane.widthProperty());
+		drawCanvas.heightProperty().bind(mainPane.heightProperty());
+		mainPane.getChildren().add(drawCanvas);
+		primaryStage.setScene(new Scene(mainPane));
+		primaryStage.show();
+	}
+
+}
