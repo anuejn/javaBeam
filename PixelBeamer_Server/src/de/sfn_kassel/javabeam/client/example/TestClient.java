b70371b26f614d52893d416d45282f560c6d178a
Birk Magnussen <birk.magnussen@online.de>
2015-06-28 03:59:21 +0200

    Enhanced the TestClient

--- PixelBeamer_Server/src/de/sfn_kassel/javabeam/client/example/TestClient.java
+++ PixelBeamer_Server/src/de/sfn_kassel/javabeam/client/example/TestClient.java
@@ -2,6 +2,8 @@ package de.sfn_kassel.javabeam.client.example;
 
 import java.awt.Color;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Random;
 
 import de.sfn_kassel.javabeam.client.JavabeamClient;
 
@@ -11,7 +13,10 @@ public class TestClient {
 		JavabeamClient beamer = new JavabeamClient("localhost");
 		
 		try {
-			beamer.drawRectangle(10, 10, 10, 10, Color.CYAN);
+			int offsetX = new Random().nextInt(600);
+			int offsetY = new Random().nextInt(600);
+			beamer.drawRectangle(10 + offsetX, 10 + offsetY, 100, 50, Color.CYAN);
+			beamer.drawText(15 + offsetX, 45 + offsetY, Color.BLACK, 20, InetAddress.getLocalHost().getHostName());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
